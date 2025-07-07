3b8cb8b784985fe05d900d78e57724793cc356ec
Ridho Pangestu <107269342+Ridho-pangestu@users.noreply.github.com>
2025-07-08 03:08:07 +0700

    Add files via upload

---
+++ dist/js/scrip.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector
+(".ri-menu-3-line");
+const menu = document.querySelector(".menu");
+
+hamburger.addEventListener("click", () => {
+    menu.classList.toggle("menu-active");
+});
+
+window.onscroll = () => {
+    menu.classList.remove("menu-active");
+
+};
